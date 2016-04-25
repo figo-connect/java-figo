b46aa7886199105e6d5270d73de80d2f2a13a457
Jan van Esdonk <jve@figo.me>
2016-04-25 10:26:03 +0200

    added getters and setters to TanScheme

--- src/main/java/me/figo/models/TanScheme.java
+++ src/main/java/me/figo/models/TanScheme.java
@@ -12,5 +12,28 @@ public class TanScheme {
 	
 	@Expose
 	private String medium_name;
-	
+
+    public String getTan_scheme_id() {
+        return tan_scheme_id;
+    }
+
+    public void setTan_scheme_id(String tan_scheme_id) {
+        this.tan_scheme_id = tan_scheme_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMedium_name() {
+        return medium_name;
+    }
+
+    public void setMedium_name(String medium_name) {
+        this.medium_name = medium_name;
+    }
 }
