989227adcf785a61e5c6f00632c99b0d8ae6d020
Yury V. Zaytsev <yury.zaytsev@moneymeets.com>
2017-01-27 11:43:59 +0100

    Remove expired `api.figo.me` fingerprint from trust store (closes #25)
    Signed-off-by: Yury V. Zaytsev <yury.zaytsev@moneymeets.com>

--- src/main/java/me/figo/internal/FigoTrustManager.java
+++ src/main/java/me/figo/internal/FigoTrustManager.java
@@ -37,8 +37,9 @@ import org.apache.commons.codec.binary.Hex;
 public class FigoTrustManager implements X509TrustManager {
 
     private static final List<String> VALID_FINGERPRINTS = new ArrayList<>(Arrays.asList(
-            "38AE4A326F16EA1581338BB0D8E4A635E727F107",                                                     
-            "DBE2E9158FC9903084FE36CAA61138D85A205D93"));
+            // api.figo.me
+            "DBE2E9158FC9903084FE36CAA61138D85A205D93"
+    ));
 
     /**
      * @return the list of trusted certificate fingerprints using SHA1
