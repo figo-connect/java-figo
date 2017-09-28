81a2834e73bc3417c64458135028cc298e570b76
Conrad Kutsch <conrad.kutsch@bcgdv.com>
2017-09-28 19:01:45 +0200

    Added test to ensure not to break api

--- src/test/java/me/figo/GsonTest.java
+++ src/test/java/me/figo/GsonTest.java
@@ -1,11 +1,13 @@
 package me.figo;
 
+import com.google.gson.Gson;
+import me.figo.internal.SetupAccountRequest;
+import me.figo.models.AdditionalTransactionInfo;
 import org.junit.Assert;
 import org.junit.Test;
 
-import com.google.gson.Gson;
-
-import me.figo.models.AdditionalTransactionInfo;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class GsonTest {
 
@@ -17,5 +19,22 @@ public class GsonTest {
 				"{\"compensation_amount\": \"6,10\",\"original_amount\": \"575.40\"}", AdditionalTransactionInfo.class);
 		Assert.assertNull(obj.getCompensation_amount());
 		Assert.assertEquals(obj.getOriginal_amount().toPlainString(), "575.40");
+	}
+
+	@Test
+	public void testSetupAccountRequestSerialization() {
+		FigoApi api = new FigoApi("", 0);
+		Gson g = api.createGson();
+
+		SetupAccountRequest testObject;
+		String expected;
+
+		testObject = new SetupAccountRequest("bankCode", "de", Arrays.asList("login", "pin"), Collections.<String>emptyList(), false, false, "uri");
+		expected = "{\"bank_code\":\"bankCode\",\"country\":\"de\",\"credentials\":[\"login\",\"pin\"],\"sync_tasks\":[],\"save_pin\":false,\"disable_first_sync\":false,\"redirect_uri\":\"uri\"}";
+		Assert.assertEquals(expected, g.toJson(testObject));
+
+		testObject = new SetupAccountRequest("bankCode", "de", "login", "pin", Collections.<String>emptyList());
+		expected = "{\"bank_code\":\"bankCode\",\"country\":\"de\",\"credentials\":[\"login\",\"pin\"],\"sync_tasks\":[],\"save_pin\":false,\"disable_first_sync\":false}";
+		Assert.assertEquals(expected, g.toJson(testObject));
 	}
 }
